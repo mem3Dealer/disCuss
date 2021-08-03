3ea272ec8a0251f5c23dfc4a5ed27c6029970de0
DMK <dkuznetsov@zfx.com>
2021-08-03 21:18:42 +0300

    Premigrate

--- lib/widgets/anotherGroupCreator.dart
+++ lib/widgets/anotherGroupCreator.dart
@@ -26,84 +26,82 @@ class AnotherGroupCreator extends StatelessWidget {
       title: Text("Create new room"),
       content: SingleChildScrollView(
         child: Container(
-          child: Column(
-            children: [
-              TextFormField(
-                controller: _textFieldController,
-                key: formKey,
-                validator: (val) =>
-                    val!.isEmpty ? "Please enter group`s name" : null,
-                decoration: InputDecoration(hintText: 'Enter group`s name...'),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              BlocBuilder<UserCubit, UserListState>(
-                bloc: userCubit,
-                builder: (context, state) {
-                  // state.selectedUsers = [];
-                  // List _list = [];
-                  return Column(
-                    children: [
-                      // (state.selectedUsers == null)
-                      //     ? Text('')
-                      //     : Text('Selected Members: ${_list.toString()}'),
-                      // SizedBox(
-                      //   height: 20,
-                      // ),
+          child: Form(
+            key: formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _textFieldController,
+                  validator: (val) => val!.isEmpty ? "Please enter group`s name" : null,
+                  decoration: InputDecoration(hintText: 'Enter group`s name...'),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                BlocBuilder<UserCubit, UserListState>(
+                  bloc: userCubit,
+                  builder: (context, state) {
+                    // state.selectedUsers = [];
+                    // List _list = [];
+                    return Column(
+                      children: [
+                        // (state.selectedUsers == null)
+                        //     ? Text('')
+                        //     : Text('Selected Members: ${_list.toString()}'),
+                        // SizedBox(
+                        //   height: 20,
+                        // ),
 
-                      // Column(
-                      //   children: [
-                      //     Text(
-                      //         "SELS ARE ${state.selectedUsers.toString()}"),
-                      //     Container(child: Text(_list.toString())),
-                      //     Text(state.version.toString()),
-                      //   ],
-                      // ),
+                        // Column(
+                        //   children: [
+                        //     Text(
+                        //         "SELS ARE ${state.selectedUsers.toString()}"),
+                        //     Container(child: Text(_list.toString())),
+                        //     Text(state.version.toString()),
+                        //   ],
+                        // ),
 
-                      Container(
-                        height: 300,
-                        width: 300,
-                        child: ListView.builder(
-                            shrinkWrap: true,
-                            itemCount: state.listUsers!.length,
-                            itemBuilder: (context, index) {
-                              // List<MyUser>? selected = state.selectedUsers;
-                              List<MyUser>? listUsers = state.listUsers!;
-                              // bool selected ;
-                              // state.selectedUsers = [];
-                              // if (state.selectedUsers!
-                              //     .contains(listUsers[index]))
-                              if (listUsers[index] ==
-                                  authCubit.state.currentUser)
-                                return SizedBox.shrink();
-                              else
-                                return ListTile(
-                                  onTap: () {
-                                    userCubit.selectUser(listUsers[index]);
-                                  },
-                                  trailing: state.selectedUsers!
-                                          .contains(listUsers[index])
-                                      // ==
-                                      //     selected.contains(listUsers[index])
-                                      ? Icon(Icons.check_box)
-                                      : Icon(Icons.check_box_outline_blank),
-                                  title: Text(
-                                    listUsers[index].name.toString(),
-                                  ),
-                                  subtitle:
-                                      Text(listUsers[index].email.toString()),
-                                );
-                            }),
-                      )
-                    ],
-                  );
-                },
-              )
-            ],
+                        Container(
+                          height: 300,
+                          width: 300,
+                          child: ListView.builder(
+                              shrinkWrap: true,
+                              itemCount: state.listUsers!.length,
+                              itemBuilder: (context, index) {
+                                // List<MyUser>? selected = state.selectedUsers;
+                                List<MyUser>? listUsers = state.listUsers!;
+                                // bool selected ;
+                                // state.selectedUsers = [];
+                                // if (state.selectedUsers!
+                                //     .contains(listUsers[index]))
+                                if (listUsers[index] == authCubit.state.currentUser)
+                                  return SizedBox.shrink();
+                                else
+                                  return ListTile(
+                                    onTap: () {
+                                      userCubit.selectUser(listUsers[index]);
+                                    },
+                                    trailing: state.selectedUsers!.contains(listUsers[index])
+                                        // ==
+                                        //     selected.contains(listUsers[index])
+                                        ? Icon(Icons.check_box)
+                                        : Icon(Icons.check_box_outline_blank),
+                                    title: Text(
+                                      '${listUsers[index].name.toString()} ++ ${authCubit.state.currentUser}',
+                                    ),
+                                    subtitle: Text(listUsers[index].email.toString()),
+                                  );
+                              }),
+                        )
+                      ],
+                    );
+                  },
+                )
+              ],
+            ),
           ),
         ),
       ),
