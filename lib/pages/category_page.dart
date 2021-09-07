945f5f2acc452af4a123c9c02cc86291dab8e768
DMK <dkuznetsov@zfx.com>
2021-09-07 16:06:17 +0300

    Polish category page

--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -4,42 +4,77 @@ import 'package:my_chat_app/cubit/cubit/room_cubit.dart';
 import 'package:my_chat_app/pages/anotherGroupCreator.dart';
 import 'package:my_chat_app/pages/home.dart';
 import 'package:my_chat_app/services/database.dart';
+import 'dart:ui';
 
 class CategoryPage extends StatelessWidget {
   /// ее вызывать на главнойто есть вместо всех чатов на хоуме будет выбор категории? да понял
   CategoryPage({Key? key}) : super(key: key);
   final data = GetIt.I.get<DataBaseService>();
   final room = GetIt.I.get<RoomCubit>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(
-          children: data.categories
-              .map((e) => ListTile(
-                  title: Text(e['label']!),
-                  onTap: () {
-                    room.setCategory(e['option']!); //ok
-                    Navigator.of(context).push(
-                      MaterialPageRoute<void>(
-                        builder: (BuildContext context) => HomePage(),
-                      ),
+    final ThemeData theme = Theme.of(context);
+    return Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              scale: theme.brightness == Brightness.dark ? 0.05 : 0.3,
+              repeat: ImageRepeat.repeat,
+              // fit: BoxFit.cover,
+              image: theme.brightness == Brightness.dark
+                  ? ExactAssetImage('assets/dark_back.png')
+                  : ExactAssetImage('assets/light_back.jpg')),
+        ),
+        child: BackdropFilter(
+          filter: theme.brightness == Brightness.dark
+              ? ImageFilter.blur(sigmaX: 13.0, sigmaY: 13.0)
+              : ImageFilter.blur(sigmaX: 3.0, sigmaY: 3.0),
+          child: Scaffold(
+            backgroundColor: Colors.transparent,
+            appBar: AppBar(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              title: Text('Select categore'),
+            ),
+            body: Container(
+              decoration: BoxDecoration(
+                  color: Theme.of(context).scaffoldBackgroundColor,
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(25.0), topRight: Radius.circular(25.0))),
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: ListView(
+                  children: data.categories
+                      .map((e) => ListTile(
+                          dense: true,
+                          leading: Icon(
+                            Icons.menu,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                          title: Text(e['label']!),
+                          onTap: () {
+                            room.setCategory(e['option']!); //ok
+                            Navigator.of(context).push(
+                              MaterialPageRoute<void>(
+                                builder: (BuildContext context) => HomePage(),
+                              ),
+                            );
+                          }))
+                      .toList()),
+            ),
+            floatingActionButton: FloatingActionButton(
+              child: Icon(Icons.add),
+              onPressed: () {
+                // print(room.state);
+                Navigator.of(context).push(MaterialPageRoute<void>(
+                        builder: (BuildContext context) =>
+                            // SliverPage()
+                            AnotherGroupCreator(false)
+                        // BackDropPage()
+                        )
+                    // )
                     );
-                  }))
-              .toList()),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          // print(room.state);
-          Navigator.of(context).push(MaterialPageRoute<void>(
-                  builder: (BuildContext context) =>
-                      // SliverPage()
-                      AnotherGroupCreator(false)
-                  // BackDropPage()
-                  )
-              // )
-              );
-        },
-      ),
-    );
+              },
+            ),
+          ),
+        ));
   }
 }
